60b7f270105eea8787035f10ffde723a52520808
Sebastien Lucas <lucas.sebastien@gmail.com>
2012-08-11 02:01:10 +0200

    detect when drag has stopped

--- drag.js
+++ drag.js
@@ -1,9 +1,13 @@
-alert('je suis chargé');
-
-
 (function ($) {
   Drupal.behaviors.dragModule = function (){
-    $('.draggable').draggable(); 
+    // the element with a class .draggable become draggable
+    $('.draggable').draggable();
+  
+    // Make something when the drag stops 
+    
+    $('.draggable').bind('dragstop', function(event, ui){
+        alert('the drag has stopped !');
+     }); 
   };
 }(jQuery));
 
